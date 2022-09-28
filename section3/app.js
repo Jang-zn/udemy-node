48a9ba5522da7dd47de0c5c7dd6a8a6549b03b07
Jang-zn <zn2309@naver.com>
2022-09-28 17:11:33 +0900

    sec3~34

--- section3/app.js
+++ section3/app.js
@@ -1,7 +1,33 @@
  const http = require('http');
+ const fs = require('fs');
 
  const server = http.createServer((req, res)=>{
-    console.log(req);
+    const url = req.url;
+    const method = req.method;
+    if(url==='/'){
+      res.write('<html> <head><title>Enter Message</title><head>');
+      res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">submit</button></form></body></html>');
+      return res.end();
+    }
+    if(url ==='/message' && method ==='POST'){
+      const body=[];
+      req.on('data',(data)=>{
+         body.push(data);
+      });
+      req.on('end', ()=>{
+         const parsedBody = Buffer.concat(body).toString();
+         const message = parsedBody.split('=')[1];
+         fs.writeFileSync('message.txt', message);
+      });
+      
+      res.statusCode=302;
+      res.setHeader('Location', '/');
+      return res.end();
+   }
+   const message = req.message;
+      res.write('<html> <head><title>your Message</title><head>');
+      res.write('<body><h1>'+message+'</h1></body></html>');
+      return res.end();
  });
 
  server.listen(2000);
