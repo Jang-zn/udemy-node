6fa8f565dde25960522126776d06c7834d7fbdc7
Jang-zn <zn2309@naver.com>
2022-11-09 17:10:48 +0900

    admin Data의 공유 - 서버 메모리에 들어가는 형태라서 다른 사용자에게도 공유되는 문제가 있다

--- section5/routes/admin.js
+++ section5/routes/admin.js
@@ -4,6 +4,8 @@ const router = express.Router();
 
 const rootDir = require('../util/path');
 
+const products = [];
+
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next)=>{
     res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
@@ -12,8 +14,9 @@ router.get('/add-product', (req, res, next)=>{
 // /admin/add-product => POST
 router.post('/add-product', (req, res, next)=>{
     console.log(req.body);
-    const title = req.body.title;
-    res.send('<h1>'+title+"</h1>");
+    products.push({title:req.body.title});
+    res.redirect("/")
 });
 
-module.exports = router;
+exports.router = router;
+exports.products = products;
--- section5/routes/shop.js
+++ section5/routes/shop.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const path = require('path');
 const rootDir = require('../util/path');
 
+const adminData =require('./admin');
 
 router.get('/', (req, res, next)=>{
     res.sendFile(path.join(rootDir,'views', 'shop.html'));
