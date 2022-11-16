80ce840dd82f359db735f3a8a5f849e2e44b5be3
dndudJang <wyjang@rbh.co.kr>
2022-11-16 16:18:08 +0900

    file 저장

--- section5/models/product.js
+++ section5/models/product.js
@@ -1,6 +1,17 @@
 const fs = require('fs');
 const path = require('../util/path')
+const p = path+'/data/products.json';
 
+//helper function
+const getProductsFromFile = (func)=>{
+        fs.readFile(p, (err, data) => {
+            if (err) {
+                func([]);
+            }else{
+                func(JSON.parse(data));
+            }
+        })
+}
 
 module.exports = class Product{
     constructor(title){
@@ -8,34 +19,24 @@ module.exports = class Product{
     }
 
     save(){
-        const p = path+'/data/products.json';
-        fs.readFile(p,(err, data)=>{
-            let products=[];
-            if(!err){
-                products = JSON.parse(data);
-            }
+        getProductsFromFile(products=>{
             products.push(this);
             fs.writeFile(p,JSON.stringify(products),(err)=>{
-                console.log(err);
+                if(err){
+                    console.log(err);
+                }
+                
             });
         });
+
     }
 
     //static 키워드 사용시 내가 아는 그 static처럼 작동함.
-    static fetchAll(){
-        const p = path+'/data/products.json';
-        let products=[];
-        fs.readFile(p,(err, data)=>{
-            if(!err){
-                products = JSON.parse(data);
-                return products
-            }
-            fs.writeFile(p,JSON.stringify(products),(err)=>{
-                console.log(err);
-            });
-            return products
-
-        })
+    //이거 async await 왜 안되는지 모르겠는데.. 암튼 callback함수를 파라미터로 넣고
+    //리턴값을 callback의 매개변수로 지정,
+    //호출하는 지점(controller)에서 callback을 구현해서 리턴값을 받아 처리하게 만든다.
+    static fetchAll(func){
+        getProductsFromFile(func);
     }
 
 }
