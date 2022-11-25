4bed2022e5b8a6972aecb191bf03a96637d05767
Jang-zn <zn2309@naver.com>
2022-11-25 14:44:34 +0900

    product model 수정

--- section5/controllers/admin.controller.js
+++ section5/controllers/admin.controller.js
@@ -5,7 +5,8 @@ exports.getAddProduct=(req, res, next)=>{
 };
 
 exports.postAddProduct=(req, res, next)=>{
-    const product = new Product(req.body.title);
+    console.log(req.body)
+    const product = new Product(req.body.title, req.body.imageUrl, req.body.description, req.body.price);
     product.save();
     res.redirect("/")
 };
