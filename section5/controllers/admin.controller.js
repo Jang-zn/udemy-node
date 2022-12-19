6a11e91b239ba842b7d494be7c3c6a0280b3578d
Jang-zn <zn2309@naver.com>
2022-12-19 18:19:20 +0900

    Edit Product 진행중 * <% %> 쓸때 줄바꿈같은거 하면 안된다 싹다 String으로 잡힘

--- section5/controllers/admin.controller.js
+++ section5/controllers/admin.controller.js
@@ -1,7 +1,7 @@
 const Product = require('../models/product');
 
 exports.getAddProduct=(req, res, next)=>{
-    res.render('admin/add-product',{pageTitle:'Add Product', path:'admin/add-product'});
+    res.render('admin/edit-product',{pageTitle:'Add Product', path:'admin/add-product', editing:false});
 };
 
 exports.postAddProduct=(req, res, next)=>{
@@ -10,6 +10,38 @@ exports.postAddProduct=(req, res, next)=>{
     product.save();
     res.redirect("/")
 };
+
+exports.getEditProduct=(req, res, next)=>{
+    const editMode = req.query.edit;
+    //쿼리스트링에 필요한 파라미터 있는지 체크
+    if(!editMode){
+        return res.redirect('/');
+    }
+    const productId = req.params.productId;
+    Product.findById(productId, product =>{
+        //제품이 있는지 없는지 검사
+        if(!product){
+            //보통은 PRODUCT_NOT_FOUND 같은 Error 던져줌
+            return res.redirect('/');
+        }
+        res.render('admin/edit-product',{
+            pageTitle : 'Edit Product',
+            path : 'admin/edit-product',
+            editing : editMode,
+            product : product
+        });    
+    });
+
+    
+};
+
+exports.postEditProduct=(req, res, next)=>{
+    console.log(req.body)
+    const product = new Product(req.body.title, req.body.imageUrl, req.body.description, req.body.price);
+    product.save();
+    res.redirect("/")
+};
+
 
 exports.getProducts=(req, res, next)=>{
     Product.fetchAll(prods=>{
--- section5/routes/admin.js
+++ section5/routes/admin.js
@@ -6,12 +6,17 @@ const adminController = require('../controllers/admin.controller');
 // /admin/add-product => GET
 router.get('/add-product', adminController.getAddProduct); 
 
-// /admin/
-router.get('/products', adminController.getProducts);
-
 // /admin/add-product => POST
 router.post('/add-product', adminController.postAddProduct);
 
+// /admin/
+router.get('/products', adminController.getProducts);
+
+// /admin/edit-product/{productId} => POST
+router.post('/edit-product/:productId', adminController.postEditProduct);
+
+// /admin/edit-product/{productId} => GET
+router.get('/edit-product/:productId', adminController.getEditProduct);
 
 
 module.exports=router;
