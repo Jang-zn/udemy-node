911c511b8a8a1d953f9f6d982a9a92395086fcd9
Jang-zn <zn2309@naver.com>
2022-12-22 02:15:05 +0900

    sequelize setting

--- section5/util/database.js
+++ section5/util/database.js
@@ -1,13 +1,13 @@
-const mysql = require('mysql2'); 
+const Sequelize = require('sequelize');
 
-//connection pool 설정
-//connection은 쿼리마다 단일, 단발성이라서 pool로 쿼리 발생시마다 pool로부터 connection을 가져와서 지속 사용 가능
-const pool = mysql.createPool({
-    host:'localhost',
-    user:'root',
-    database:'udemy_node',
-    password:'wkddndud1!'
-});
+const sequelize = new Sequelize(
+    'udemy_node', 
+    'root', 
+    'wkddndud1!',
+    {
+        dialect : 'mysql',
+        host : 'localhost'
+    }
+);
 
-//.promise() 사용해서 비동기적 데이터 처리 및 promise chaining 사용 가능
-module.exports = pool.promise();
+module.exports = sequelize;
