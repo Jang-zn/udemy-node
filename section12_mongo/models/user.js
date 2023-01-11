90a794caca4b0b769eaa7202e7ef0fa7851d49c6
Jang-zn <zn2309@naver.com>
2023-01-12 00:13:31 +0900

    user model 추가

--- section12_mongo/models/user.js
+++ section12_mongo/models/user.js
@@ -1,21 +1,38 @@
-const Sequelize = require('sequelize');
-const sequelize = require('../util/database');
+const mongodb = require('mongodb');
+const getDB = require('../util/database').getDB;
 
-const User = sequelize.define('user',{
-    id:{
-        type : Sequelize.INTEGER,
-        primaryKey : true,
-        allowNull : false,
-        autoIncrement:true
-    },
-    name:{
-        type : Sequelize.STRING,
-        allowNull:false,
-    },
-    email:{
-        type : Sequelize.STRING,
-        allowNull : false,
-    },
-});
+
+class User {
+    constructor(name, email, id){
+        this.name = name,
+        this.eamil = email,
+        this._id = id ? new mongodb.ObjectId(id) : null
+    }
+
+    save(){
+        const db = getDB();
+        let user;
+        if(this._id){
+            user = db.collection('users').updateOne({_id:this._id},this);
+        }else{
+            user = db.collection('users').insertOne(this);
+        }
+        user.then(result=>{
+            // console.log()
+        })
+        .catch(err=>{console.log(err)});
+    }
+
+    static findById(userId){
+        const db = getDB();
+        return db.collection('users').find({_id: new mongodb.ObjectId(userId)}).next()
+        .then(user =>{
+            return user;
+        })
+        .catch(err=>{
+            console.log(err);
+        })
+    }
+}
 
 module.exports = User;
